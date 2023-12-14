0b985b33b7e6385ab42872cfb30ae4b98aa5fd74
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-12-13 22:13:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,23 @@ with open('mobs.json', 'r') as file:
 def get_monsters():
     return jsonify({'monsters': monsters})
 
-# Rota para obter informações sobre um mob específico
+# Rota para obter informações sobre um mob específico por id
 @app.route('/monsters/<int:monster_id>', methods=['GET'])
 def get_monster(monster_id):
     monster = next((m for m in monsters if m['id'] == monster_id), None)
     if monster:
         return jsonify({'monster': monster})
     return jsonify({'message': 'Mob não encontrado'}), 404
+
+
+# Rota para obter informações sobre um mob específico pelo level
+@app.route('/monsters/level/<int:monster_level>', methods=['GET'])
+def get_monster_by_level(monster_level):
+    monsters_by_level = [m for m in monsters if m['Level'] == monster_level]
+    if monsters_by_level:
+        return jsonify({'monsters': monsters_by_level})
+    return jsonify({'message': 'Nenhum mob encontrado para o nível especificado'}), 404
+
 
 # Rota para adicionar um novo mob
 @app.route('/monsters', methods=['POST'])
